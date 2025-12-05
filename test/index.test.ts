ffbad829a0ae9c9ab943f26347291a31c3f973a9
cv <1018496570@qq.com>
2025-12-05 17:46:32 +0800

    更新 index.test.ts，重构测试用例以验证 SelectBuilder 的简单选择功能

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,7 +1,20 @@
 import { describe, expect, it } from 'vitest'
+import { F, SelectBuilder } from '../src/index'
 
-describe('should', () => {
-  it('exported', () => {
-    expect(1).toEqual(1)
+describe('influxql-builder', () => {
+  it('builds simple select', () => {
+    const q = new SelectBuilder()
+      .select(F.count('output'))
+      .from('product_record')
+      .where(w =>
+        w.andEq('qualified', '1')
+          .andEq('step', 'final')
+          .between('time', ['2025-12-03T16:00:00.000Z', '2025-12-04T15:59:00.000Z']),
+      )
+      .toString()
+    expect(q)
+      .toBe(
+      /* sql */`SELECT COUNT(output) FROM product_record WHERE (qualified = '1') AND (step = 'final') AND (time >= '2025-12-03T16:00:00.000Z' AND time <= '2025-12-04T15:59:00.000Z')`,
+      )
   })
 })
